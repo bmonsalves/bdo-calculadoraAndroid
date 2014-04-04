aab421997088b70fa500aab300e1ef7a4259fbc7
bmonsalves <bdo.monsalves@gmail.com>
2014-04-03 22:38:53 -0300

    apk y fija posicion de pantalla

--- app/src/main/java/cl/backapp/calculadora/app/MainActivity.java
+++ app/src/main/java/cl/backapp/calculadora/app/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +17,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         Button Button0 = (Button) findViewById(R.id.Button0);
